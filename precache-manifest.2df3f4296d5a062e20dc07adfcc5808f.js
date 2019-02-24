d9fde0e10fe50c559c0796512c7c2154d3af2fb5
BrotherFatcake <BrotherFatcake@brownscastle.com>
2019-02-24 16:31:29 -0600

    Updates

---
+++ precache-manifest.2df3f4296d5a062e20dc07adfcc5808f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "780bcbc715c88212df47",
+    "url": "/portfolio-react/static/js/runtime~main.780bcbc7.js"
+  },
+  {
+    "revision": "7964c6b6ba3906c58eec",
+    "url": "/portfolio-react/static/js/main.7964c6b6.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/portfolio-react/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "7964c6b6ba3906c58eec",
+    "url": "/portfolio-react/static/css/main.b7adfa9d.chunk.css"
+  },
+  {
+    "revision": "9c1ee3edb09c5db36adb70234c01e08d",
+    "url": "/portfolio-react/index.html"
+  }
+];
