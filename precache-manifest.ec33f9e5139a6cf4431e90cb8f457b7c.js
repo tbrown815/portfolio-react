817fd0f57d761be272acd1cdda9dc22be4dfb985
BrotherFatcake <BrotherFatcake@brownscastle.com>
2019-03-03 09:14:21 -0600

    Updates

---
+++ precache-manifest.ec33f9e5139a6cf4431e90cb8f457b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7331ec94a1816fdaa512",
+    "url": "tbrowndev.brownscastle.com/static/js/runtime~main.7331ec94.js"
+  },
+  {
+    "revision": "2c51611a280c2c83d8ff",
+    "url": "tbrowndev.brownscastle.com/static/js/main.2c51611a.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "tbrowndev.brownscastle.com/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "2c51611a280c2c83d8ff",
+    "url": "tbrowndev.brownscastle.com/static/css/main.4cb1fb04.chunk.css"
+  },
+  {
+    "revision": "cfedc773303bb2074119e5ca5f8161ed",
+    "url": "tbrowndev.brownscastle.com/index.html"
+  }
+];
